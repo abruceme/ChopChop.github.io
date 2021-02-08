a760963961c99198c8f59f99738b526390ddf364
Abeeku Bruce-Mensah <brucemen@usc.edu>
2021-02-07 23:51:01 -0800

    Create block and player and enemy attack animations create enemy idle, hold and attack animations create player block, idle and attack animation create player move script to use wad for top, left, right attack add spacebar attack to player move script add enum for player animation: 0 is idle 1 is rightslash 2 is leftslash 3 is rightblock 4 is upblock 5 is leftblock

---
+++ ChopChop/Assets/Scripts/Player/PlayerMoveset.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveset : MonoBehaviour
+{
+    Animator animator;
+    public enum PlayerStates
+    {
+        IDLE,
+        RIGHTSLASH,
+        LEFTSLASH,
+        RIGHTBLOCK,
+        UPBLOCK,
+        LEFTBLOCK
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animator.GetInteger("Move") == (int)PlayerStates.RIGHTSLASH)
+        {
+            animator.SetInteger("Move", (int)PlayerStates.IDLE);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetInteger("Move", (int)PlayerStates.LEFTBLOCK);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetInteger("Move", (int)PlayerStates.UPBLOCK);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetInteger("Move", (int)PlayerStates.RIGHTBLOCK);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(animator.GetInteger("Move"));
+            animator.SetInteger("Move", (int)PlayerStates.RIGHTSLASH);
+        }
+    }
+}
