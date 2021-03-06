7f4c5d2482191f25fa813fbb9dcc27c71cb42fd1
Abeeku Bruce-Mensah <brucemen@usc.edu>
2021-03-06 12:17:37 -0800

    Add analytics line to track number of enemies killed

--- ChopChop/Assets/Scripts/Health.cs
+++ ChopChop/Assets/Scripts/Health.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class Health : MonoBehaviour
 {
@@ -57,6 +58,8 @@ public class Health : MonoBehaviour
                 {
                     Destroy(gameCharacter.parent.gameObject);
                     gameCharacter.GetComponentInParent<EnemySpawner>().ResetTimeBetweenNextSpawn();
+                    AnalyticsResult analyticsResult = Analytics.CustomEvent("Destroy");
+                    Debug.Log("analyticsResult: " + analyticsResult);
                 }
                 if(characterTag == "Player")
                 {
