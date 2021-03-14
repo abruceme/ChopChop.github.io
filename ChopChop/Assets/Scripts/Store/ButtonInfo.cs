65ae04a5f5beee87ae7f0a9ea6abbc35fbe07b33
Haotian <yonggandexin68@126.com>
2021-03-14 17:46:10 +0800

    create a simple store and modify the rotate player to rotate 90 degrees each time
    1. for store , create a health potion and power potion, and each purchase will cost some points.
    2. modify the rotate function to rotate 90 degrees every time press the leftarrow and the rightarrow.

---
+++ ChopChop/Assets/Scripts/Store/ButtonInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonInfo : MonoBehaviour
+{
+    public int boostID;
+    public Text priceText;
+    public GameObject StoreManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        priceText.text = "Price: $" + StoreManager.GetComponent<StoreManagerScript>().storeItems[2, boostID].ToString();
+        
+    }
+}
---
+++ ChopChop/Assets/Scripts/Store/StoreManagerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class StoreManagerScript : MonoBehaviour
+{
+    public int[,] storeItems = new int[3,3];
+    public float points;
+    public Text pointsText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointsText.text = "Points:" + points.ToString();
+
+        //item id
+        storeItems[1, 1] = 1;       //health potion id
+        storeItems[1, 2] = 2;       //add attack boost id
+
+        //item price
+        storeItems[2, 1] = 30;      //health potion price
+        storeItems[2, 2] = 50;      //add attack boost price
+
+    }
+
+    // Update is called once per frame
+    public void BuyBoosts()
+    {
+        GameObject buttonRef = GameObject.FindGameObjectWithTag("StoreEvent").GetComponent<EventSystem>().currentSelectedGameObject;
+        // Debug.Log("TESTTESTTTTTTTTTTTTTTTTTTTT");
+        Debug.Log("yes cannnn buy " + storeItems[2, buttonRef.GetComponent<ButtonInfo>().boostID]);
+        if(points >= storeItems[2, buttonRef.GetComponent<ButtonInfo>().boostID]){
+            points -= storeItems[2, buttonRef.GetComponent<ButtonInfo>().boostID];
+            Debug.Log(points);
+            pointsText.text = "Points:" + points.ToString();
+        }
+    }
+}
