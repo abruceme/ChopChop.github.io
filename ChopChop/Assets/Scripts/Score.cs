0e894c7faac7867721bbbb8075717ab710c1c316
ajmcgrath <mcgratha@usc.edu>
2021-03-14 22:17:33 -0400

    Adding scoring

---
+++ ChopChop/Assets/Scripts/Score.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace scoring
+{
+
+    public class Score : MonoBehaviour
+    {
+
+        public Text scoreText;
+        public Text goldText;
+        private static int totalPoints;
+        private static int totalGold;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            totalPoints = 0;
+            totalGold = 0;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            scoreText.text = "<color=#0D3CD7>" + totalPoints.ToString() + "</color>";
+            goldText.text = "<color=#D79F0D>" + totalGold.ToString() + "</color>";
+        }
+
+        public static void addScore(int gold, int points)
+        {
+            totalPoints += points;
+            totalGold += gold;
+        }
+
+        public static void useGold(int gold)
+        {
+            totalGold -= gold;
+        }
+
+        public static int getTotalScore()
+        {
+            return totalPoints;
+        }
+
+        public static int getGold()
+        {
+            return totalGold;
+        }
+    }
+}
