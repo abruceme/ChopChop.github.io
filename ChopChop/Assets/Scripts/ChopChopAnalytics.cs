46d05b4f96e8ef790f712ff436463f220e99bb7f
zohrehazizi <61987455+zohrehazizi@users.noreply.github.com>
2021-03-15 20:28:40 -0700

    custom event for steal weapon fixed

--- ChopChop/Assets/Scripts/ChopChopAnalytics.cs
+++ ChopChop/Assets/Scripts/ChopChopAnalytics.cs
@@ -66,6 +66,10 @@ public class ChopChopAnalytics : MonoBehaviour
         });
         Debug.Log("Result: " + result);
 
+        AnalyticsResult result2 = Analytics.CustomEvent("AvgGameSession2", new Dictionary<string, object>{
+            {"WeaponStolen", stealweapon});
+        Debug.Log("Result: " + result2);
+
     }
     public void IncrementLeftAttack()
     {
